a67f8569b4a8cabee810d3640b4a081bd4c0b580
Luca Renzi <S1098311@studenti.univpm.it>
2021-12-15 18:29:02 +0100

    modifica classe controller

--- TicketmasterEsameOOP/src/main/java/it/univpm/TicketmasterEsameOOP/controller/Controller.java
+++ TicketmasterEsameOOP/src/main/java/it/univpm/TicketmasterEsameOOP/controller/Controller.java
@@ -1,25 +1,32 @@
 package it.univpm.TicketmasterEsameOOP.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.univpm.TicketmasterEsameOOP.model.Event;
-import it.univpm.TicketmasterEsameOOP.service.Service;
+import it.univpm.TicketmasterEsameOOP.service.*;
 
+import it.univpm.TicketmasterEsameOOP.model.*;
 @RestController
 public class Controller {
 	
 	//@Autowired
 	private Service service;
 
-	@RequestMapping(value="/events")
-	public ResponseEntity<Object> getJSONEvent(@RequestParam(name = "countryCode", defaultValue = "PL")String country){
+	@RequestMapping(value="/discovery/v2/events&countryCode=country")
+	public ResponseEntity<Object> getJSONEvent(@PathVariable String country ){
+		Event e= 
 		return new ResponseEntity<>(service.toJSON(service.getEvent(service.getJSONEvents("PL"))), HttpStatus.OK);
 	}
-	
+	/*@RequestMapping(value="/discovery/v2/events")
+	public ResponseEntity<Object> getJSONEvent(@RequestParam(name = "countryCode", defaultValue = "PL")String country){
+		return new ResponseEntity<>(service.toJSON(service.getEvent(service.getJSONEvents("PL"))), HttpStatus.OK);
+	}*/
 }
