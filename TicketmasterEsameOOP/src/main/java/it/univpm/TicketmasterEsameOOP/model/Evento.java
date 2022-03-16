42dc3ae77d9ab90af8b2ff0f00239ad10d65947f
Luca Renzi <S1098311@studenti.univpm.it>
2022-03-16 14:35:00 +0100

    modifica classe evento

--- TicketmasterEsameOOP/src/main/java/it/univpm/TicketmasterEsameOOP/model/Evento.java
+++ TicketmasterEsameOOP/src/main/java/it/univpm/TicketmasterEsameOOP/model/Evento.java
@@ -6,6 +6,11 @@ public class Evento {
 	private Vector<String> generi;
 	private Vector<String> stati;
 
+	public Evento(Vector<String> stati, Vector<String> generi) {
+		this.generi=generi;
+		this.stati=stati;
+	}
+	
 	public Vector<String> getGeneri() {
 		return generi;
 	}
